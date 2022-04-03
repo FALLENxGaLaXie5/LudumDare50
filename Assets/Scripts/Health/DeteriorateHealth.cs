f83c3efe1de4d3542e3ee46063f6b04788348405
DESKTOP-7BGJ7RJ\stewa <steward.joshua@rocketmail.com>
2022-04-02 19:58:21 -0700

    damage to player is ccntrollever time, more animations implemented

---
+++ Assets/Scripts/Health/DeteriorateHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DeteriorateHealth : MonoBehaviour
+{
+    [SerializeField][Range(5f, 15f)] private float minTimeBetweenDeterioration = 10f;
+    [SerializeField][Range(15f, 30f)] private float maxTimeBetweenDeterioration = 25f;
+    [SerializeField] private int deteriorationStepAmount = 5;
+    private Health health;
+    
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Deteriorate());
+    }
+
+    private IEnumerator Deteriorate()
+    {
+        while (health.CurrentHealth > 0)
+        {
+            yield return new WaitForSeconds(Random.Range(minTimeBetweenDeterioration, maxTimeBetweenDeterioration));
+            health.Damage(deteriorationStepAmount, gameObject, 0.3f, 0.1f, Vector3.up);
+        }
+        yield return null;
+    }
+}
